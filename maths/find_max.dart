437539d3434e9828134006b82e14f7af545578a3
Stephen Curry <m18824909883@163.com>
2019-09-08 17:25:37 +0800

    Create find_max.dart

---
+++ maths/find_max.dart
@@ -0,0 +1,15 @@
+//find the max number
+find_max(List numbers){
+  var max = numbers[0];
+  for(var x in numbers){
+    if(x > max){
+      max = x;
+    }
+  }
+ print(max);
+}
+
+void main(){
+  find_max([2, 9, 18, 8, 76, -3]);
+}
+
