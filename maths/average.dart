2a4b226d54bb4c08a275027147af33f69cf79b73
Stephen Curry <m18824909883@163.com>
2019-09-08 17:25:08 +0800

    Create average.dart

---
+++ maths/average.dart
@@ -0,0 +1,15 @@
+//Find mean of a list of numbers.
+
+average(List numbers){
+  var sum = 0;
+  for(var x in numbers){
+    sum += x;
+  }
+  var avg = sum / numbers.length;
+  print(avg);
+  return avg;
+}
+
+void main(){
+  average([2, 8, 6, 90, 60]);
+}
