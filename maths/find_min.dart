b2f5348cbb5ea0e5069f7d83c173f70fc99dd892
Stephen Curry <m18824909883@163.com>
2019-09-08 17:24:32 +0800

    Create find_min.dart

---
+++ maths/find_min.dart
@@ -0,0 +1,14 @@
+//find the min number
+find_min(List numbers){
+  var min = numbers[0];
+  for(var x in numbers){
+    if(min > x){
+      min = x;
+    }
+  }
+ print(min);
+}
+
+void main(){
+  find_min([2, 9, 18, 8, 76, -3]);
+}
