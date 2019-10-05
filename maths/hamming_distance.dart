2212f0dae87e79d7778efb421e026bdc35bfe098
cozek <kaushikamardas@gmail.com>
2019-10-05 16:43:02 +0530

    Adding hamming distance

---
+++ maths/hamming_distance.dart
@@ -0,0 +1,39 @@
+// The Hamming distance between two strings of equal length is
+// the number of positions at which the corresponding symbols are different.
+// https://en.wikipedia.org/wiki/Hamming_distance
+
+int hamming_distance(String stringA, String stringB) {
+  //Calculates Hamming Distance
+  int distance;
+
+  //strings must be of equal length
+  if (stringA.length != stringB.length) {
+    print('String lengths must be same!');
+    return null;
+  } else {
+    distance = 0;
+    for (var i = 0; i < stringA.length; i++) {
+      if (stringA[i] != stringB[i]) {
+        distance += 1;
+      }
+    }
+  }
+  return distance;
+}
+
+void main() {
+  var stringA;
+  var stringB;
+  var dist;
+
+  stringA = 'karolin';
+  stringB = 'kathrin';
+  dist = hamming_distance(stringA, stringB);
+  print('Hamming Distance between $stringA and $stringB is $dist');
+
+  stringA = '1011101';
+  stringB = '1001001';
+  dist = hamming_distance(stringA, stringB);
+  print('Hamming Distance between $stringA and $stringB is $dist');
+}
+
