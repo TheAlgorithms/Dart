1ae4cff533d1eb46289a06a2c18e7d96ce18e7ba
mohammadreza490 <47437328+mohammadreza490@users.noreply.github.com>
2020-09-17 13:36:20 +0100

    Update binary_to_decimal.dart

--- conversions/binary_to_decimal.dart
+++ conversions/binary_to_decimal.dart
@@ -1,7 +1,7 @@
 import "dart:math" show pow;
 
 void main() {
-	print(binary_to_decimal("111")); // 7
+  print(binary_to_decimal("-111")); // -7
   print(binary_to_decimal(" 101011 ")); // 43
   print(binary_to_decimal("1a1")); //error
 }
@@ -11,6 +11,8 @@ int binary_to_decimal(String bin_string){
   if(bin_string == null || bin_string == ""){
    throw Exception("An empty value was passed to the function");
   }
+  bool isNegative = bin_string[0]=="-";
+  if(isNegative) bin_string = bin_string.substring(1);
   int decimal_val = 0;
   for (int i = 0; i<bin_string.length;i++){
     if ("01".contains(bin_string[i]) == false){
@@ -19,5 +21,5 @@ int binary_to_decimal(String bin_string){
       decimal_val += pow(2,bin_string.length - i -1)*int.parse((bin_string[i]));
     }
   }
-  return decimal_val;
+  return isNegative?-1* decimal_val: decimal_val;
 }
