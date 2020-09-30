f92f9341b460bad6a7a57143aed45c980c9ae6f3
Abhishek Maletha <67141747+Abhishek-photon@users.noreply.github.com>
2020-10-01 01:58:01 +0530

    Create Armstrong_number.dart

---
+++ maths/Armstrong_number.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+void Armstrong_no(var x)
+{
+  var n=x;
+  var s =n.toString();
+  var d=s.length;
+  var sum=0;
+  while(n!=0)
+  {
+    var r=n%10;
+    sum = sum + pow(r,d);
+    n=n~/10;
+  }
+  if(sum == x){
+    print('yes');
+  }
+  else{
+    print('no');
+  }
+}
+void main(){
+  
+  var x  = 10;
+  Armstrong_no(x);
+  
+}
