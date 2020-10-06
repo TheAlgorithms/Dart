2f9837737981ec0edecdacc6abc53589dc106f44
Abhishek Maletha <67141747+Abhishek-photon@users.noreply.github.com>
2020-10-06 12:59:07 +0530

    Create shreedharacharya.dart

---
+++ maths/shreedharacharya.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+//this function return a list of roots of a quadratic equation
+// [x1, x2] where x1 and x2 are roots of
+// aX^2 + bX + c = 0
+List shreedharacharya(double a,double b,double c) 
+{
+  double d = b*b - 4*a*c;
+  List A=[0,0];
+  if(d<0)
+  {
+    print('Imaginary roots');
+  }
+  else{
+    A[0] = (-b + pow(d,0.5))/(2*a);
+    A[1] = (-b - pow(d,0.5))/(2*a);
+    return A;
+  }
+  
+}
+void main()
+{
+  double a=1.00,b=0.00,c=-4.00;
+    List p=shreedharacharya(a,b,c);
+  print(p);
+  
+}
