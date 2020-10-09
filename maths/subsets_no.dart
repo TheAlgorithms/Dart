c9e294071ddc6d0d9fc44e1832e28d4b7f12ccfa
mohammadreza490 <47437328+mohammadreza490@users.noreply.github.com>
2020-10-09 23:24:42 +0100

    Update subsets_no.dart
    Changed the subs(n) function to only one line which makes the function cleaner and easier to understand

--- maths/subsets_no.dart
+++ maths/subsets_no.dart
@@ -1,11 +1,8 @@
 import 'dart:math';
-int subs(int a)//calculates no. of subsets of a set
-{
-  var n = pow(2,a);
-  return n;
-}
+
+int subs(int a) => pow(2,a); //calculates no. of subsets of a set
+  
 void main(){
   int n=5;
-    print(subs(n));
-  
+    print(subs(n)); 
 }
