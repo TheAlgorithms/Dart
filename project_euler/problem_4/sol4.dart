cb8293c394784a97e198fabcb81c253b608fc577
Mauricio Flores Hernandez <mau1361317@gmail.com>
2020-10-10 15:45:35 -0500

    Problem description

--- project_euler/problem_4/sol4.dart
+++ project_euler/problem_4/sol4.dart
@@ -1,4 +1,15 @@
 
+// Author : Devmaufh
+// Email : mau1361317@gmail.com
+
+/**
+ * [Problem 4](https://projecteuler.net/problem=4) solution
+ * Problem Statement:
+ * A palindromic number reads the same both ways.
+ * The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+
 void main(){
   int max = 0;
   for(int i = 100; i<1000;i++){
--- project_euler/problem_5/sol5.dart
+++ project_euler/problem_5/sol5.dart
@@ -1,3 +1,13 @@
+// Author : Devmaufh
+// Email : mau1361317@gmail.com
+
+/**
+ * [Problem 5](https://projecteuler.net/problem=5) solution
+ * Problem Statement:
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ */
+
 void main(){
   int number = 21;
   LOOP: while(true){
@@ -5,10 +15,10 @@ void main(){
     for(int i = 1; i<=21;i++){
       if( number % i !=0 ){
         isSolution = false;
-        break;
+        break; // Break if have a remainder
       } 
     }
-    if(isSolution) break LOOP;
+    if(isSolution) break LOOP; //If any number hasn't a remainder, break main loop because we found the solution
     number++;
   }
   print("Solution problem 5 = $number");
