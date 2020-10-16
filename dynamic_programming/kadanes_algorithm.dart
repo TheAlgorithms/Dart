20de67573558f017652cc53114cd23b18a6716b9
Akash G Krishnan <krishnanag1996@gmail.com>
2020-10-16 15:47:11 +0530

    Added Kadanes Algorithm
    Added in Unit test cases
    Made changes related to maxFunction

--- dynamic_programming/kadanes_algorithm.dart
+++ dynamic_programming/kadanes_algorithm.dart
@@ -1,3 +1,4 @@
+import 'package:test/test.dart';
 import 'dart:math';
 
 int kadanesAlgorithm(List<int> array) {
@@ -5,30 +6,41 @@ int kadanesAlgorithm(List<int> array) {
   int maxSoFar = array[0];
 
   for (int num in array.sublist(1, array.length)) {
-    maxEndingHere = [maxEndingHere + num, num].reduce(max);
-    maxSoFar = [maxSoFar, maxEndingHere].reduce(max);
+    maxEndingHere = max(maxEndingHere + num, num);
+    maxSoFar = max(maxSoFar, maxEndingHere);
   }
   return maxSoFar;
 }
 
 void main() {
-  List<int> array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4];
+  List<int> array;
   int maxContiniousSubarraySum;
-  maxContiniousSubarraySum = kadanesAlgorithm(array);
-  print('${maxContiniousSubarraySum == 19}');
 
-  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  maxContiniousSubarraySum;
-  maxContiniousSubarraySum = kadanesAlgorithm(array);
-  print('${maxContiniousSubarraySum == 55}');
+  test(('.Check the response for each test case'), () {
+    array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4];
 
-  array = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10];
-  maxContiniousSubarraySum;
-  maxContiniousSubarraySum = kadanesAlgorithm(array);
-  print('${maxContiniousSubarraySum == -1}');
+    maxContiniousSubarraySum = kadanesAlgorithm(array);
+    expect(maxContiniousSubarraySum, equals(19));
+  });
 
-  array = [1, 2, 3, 4, 5, 6, -22, 7, 8, 9, 10];
-  maxContiniousSubarraySum;
-  maxContiniousSubarraySum = kadanesAlgorithm(array);
-  print('${maxContiniousSubarraySum == 34}');
+  test(('.Check the response for each test case'), () {
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    maxContiniousSubarraySum;
+    maxContiniousSubarraySum = kadanesAlgorithm(array);
+    expect(maxContiniousSubarraySum, equals(55));
+  });
+
+  test(('.Check the response for each test case'), () {
+    array = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10];
+    maxContiniousSubarraySum;
+    maxContiniousSubarraySum = kadanesAlgorithm(array);
+    expect(maxContiniousSubarraySum, equals(-1));
+  });
+
+  test(('.Check the response for each test case'), () {
+    array = [1, 2, 3, 4, 5, 6, -22, 7, 8, 9, 10];
+    maxContiniousSubarraySum;
+    maxContiniousSubarraySum = kadanesAlgorithm(array);
+    expect(maxContiniousSubarraySum, equals(34));
+  });
 }
