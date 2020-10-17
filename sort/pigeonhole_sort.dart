ae603297d0705a406ec6279b2cb5a096cea7987a
Vishnu <vishnupothan2@gmail.com>
2020-10-17 20:58:15 +0530

    Foramted the dart file

--- sort/pigeonhole_sort.dart
+++ sort/pigeonhole_sort.dart
@@ -6,35 +6,29 @@ void pigeonholeSort(List arr) {
   int min = arr[0];
   int max = arr[0];
 
-  for (int i = 1; i < n; i++)
-  {
-    if (arr[i] < min)
-      min = arr[i];
-    if (arr[i] > max)
-      max = arr[i];
+  for (int i = 1; i < n; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
   }
 
   int range = max - min;
   range++;
 
   List phole = new List(range);
-  for(int i =0; i<range ; i++) {
+  for (int i = 0; i < range; i++) {
     phole[i] = 0;
   }
 
   //Populate the pigeonholes.
-  for(int i = 0; i<n; i++) {
+  for (int i = 0; i < n; i++) {
     phole[arr[i] - min];
     phole[arr[i] - min] = phole[arr[i] - min] + 1;
   }
 
   //Put the elements back into the array in order
   int index = 0;
-  
-  for(int j = 0; j<range; j++)
-    while(phole[j]-->0)
-      arr[index++]=j+min;
 
+  for (int j = 0; j < range; j++) while (phole[j]-- > 0) arr[index++] = j + min;
 }
 
 void main() {
