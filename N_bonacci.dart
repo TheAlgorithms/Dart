a9a7b80f06be0733d6e61a2f58cea593148d8c51
Neha Hasija <nehahasija9@gmail.com>
2020-10-18 16:24:19 +0530

    Create N_bonacci.dart
    This Pull Request is for HacktoberFest 2020

---
+++ N_bonacci.dart
@@ -0,0 +1,21 @@
+void N_bonacci(int n, int m) {
+  var v = new List(m);
+  var i;
+  for (i = 0; i < m; i++) {
+    v[i] = 0;
+  }
+  v[n - 1] = 1;
+  v[n] = 1;
+  for (i = n + 1; i < m; i++) {
+    v[i] = 2 * v[i - 1] - v[i - 1 - n];
+  }
+
+  print(v);
+}
+
+void main() {
+  N_bonacci(5, 10);
+  N_bonacci(4, 12);
+  N_bonacci(6, 13);
+  N_bonacci(8, 19);
+}
