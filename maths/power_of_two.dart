60b24c4c4803f81477c14a6a92e04f56c102744d
Neha Hasija <nehahasija9@gmail.com>
2020-10-18 19:56:27 +0530

    Create power_of_two.dart
    This pull request is for HacktoberFest 2020
    In this I have checked if a number is a power of 2 or not, without using logarithmic operations and by using bit manipulation.

---
+++ maths/power_of_two.dart
@@ -0,0 +1,17 @@
+void power_of_two(int n) {
+  int result = n & (n - 1);
+  if (result == 0)
+    print("Yes, the number $n is a power of 2");
+  else
+    print("No, the number $n is not a power of 2");
+}
+
+void main() {
+  power_of_two(10);
+  power_of_two(23);
+  power_of_two(32);
+  power_of_two(2234);
+  power_of_two(2048);
+
+  return;
+}
