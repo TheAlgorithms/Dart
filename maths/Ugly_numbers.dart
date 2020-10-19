454e9d217f2783313c58ffd3ffb6d4870f87f260
Miracle <73017979+RimjhimGupta@users.noreply.github.com>
2020-10-19 17:20:30 +0530

    Create Ugly_numbers.dart
    This pull request is for Hacktober Fest 2020

---
+++ maths/Ugly_numbers.dart
@@ -0,0 +1,37 @@
+int maxDivide(int a, int b) {
+  var n;
+  while (a % b == 0) {
+    n = a / b;
+    a = n.toInt();
+  }
+  return a;
+}
+
+/* Function to check if a number is ugly or not */
+int isUgly(int no) {
+  no = maxDivide(no, 2);
+  no = maxDivide(no, 3);
+  no = maxDivide(no, 5);
+
+  return (no == 1) ? 1 : 0;
+}
+
+/* Function to get the nth ugly number*/
+int getNthUglyNo(int n) {
+  int i = 1;
+  int count = 1; /* ugly number count */
+
+/*Check for all integers untill ugly count 
+	becomes n*/
+  while (n > count) {
+    i++;
+    if (isUgly(i) == 1) count++;
+  }
+  return i;
+}
+
+/* Driver program to test above functions */
+void main() {
+  var no = getNthUglyNo(150);
+  print('150th ugly no. is $no');
+}
