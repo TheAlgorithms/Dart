97481d85cbd3103137194aedee8df01db9996607
Parowicz <arturparowicz@gmail.com>
2020-10-19 18:30:02 +0200

    Add tests

--- search/peak_element.dart
+++ search/peak_element.dart
@@ -1,3 +1,5 @@
+import 'package:test/test.dart';
+
 int findPeakUtil(List<int> arr, int low, int high, int n) {
   var x = (low + high) / 2;
   int mid = x.toInt();
@@ -15,12 +17,33 @@ int findPeak(List<int> arr, int n) {
 }
 
 void main() {
-  List<int> a = [1, 3, 20, 6, 1, 2];
-  print(findPeak(a, a.length));
-  List<int> b = [1, 3, 20, 32, 1, 2];
-  print(findPeak(b, b.length));
-  List<int> c = [321, 4353, 22320, 232, 23, 223];
-  print(findPeak(c, c.length));
-  List<int> d = [121, 54, 2100, 36, 155, 90];
-  print(findPeak(d, d.length));
+  test("findPeak returns 2 for [1, 3, 20, 6, 1, 2]", () {
+    List<int> lst = [1, 3, 20, 6, 1, 2];
+    expect(findPeak(lst, lst.length), equals(2));
+  });
+
+  test("findPeak returns 3 for [1, 3, 20, 32, 1, 2]", () {
+    List<int> lst = [1, 3, 20, 32, 1, 2];
+    expect(findPeak(lst, lst.length), equals(3));
+  });
+
+  test("findPeak returns 2 for [321, 4353, 22320, 232, 23, 223]", () {
+    List<int> lst = [321, 4353, 22320, 232, 23, 223];
+    expect(findPeak(lst, lst.length), equals(2));
+  });
+
+  test("findPeak returns 2 for [121, 54, 2100, 36, 155, 90]", () {
+    List<int> lst = [121, 54, 2100, 36, 155, 90];
+    expect(findPeak(lst, lst.length), equals(2));
+  });
+
+  test("findPeak returns 2 for [5, 10, 20, 15]", () {
+    List<int> lst = [5, 10, 20, 15];
+    expect(findPeak(lst, lst.length), equals(2));
+  });
+
+  test("findPeak returns 1 for [10, 20, 15, 2, 23, 90, 67]", () {
+    List<int> lst = [10, 20, 15, 2, 23, 90, 67];
+    expect(findPeak(lst, lst.length), equals(1));
+  });
 }
