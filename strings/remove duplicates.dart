09e569b29818d78d5bd7fc41ac6545d2afd164c3
Ganesh Agarwal <69008792+Ganesh-Agarwal-25@users.noreply.github.com>
2020-10-21 01:39:11 +0530

    Update remove duplicates.dart
    Time complexity is changed to O(N).

--- strings/remove duplicates.dart
+++ strings/remove duplicates.dart
@@ -2,24 +2,14 @@ import 'package:test/test.dart';
 
 // function to remove duplicate in string
 String removeDups(String s) {
-  int j = 0;
+  var arr = new List.filled(256, 0);
   String l = '';
   int i = 0;
-  var check = 0;
   for (i = 0; i < s.length; i++) {
-    check = 0;
-    var c = s[i];
-    if (l.length != 0) {
-      for (j = 0; j < l.length; j++) {
-        if (c == l[j]) {
-          check = 1;
-          break;
-        } else
-          check = 0;
-      }
-      if (check == 0) l += c;
-    } else
-      l += c;
+    if (arr[s.codeUnitAt(i)] == 0) {
+      l += s[i];
+      arr[s.codeUnitAt(i)]++;
+    }
   }
   return l;
 }
