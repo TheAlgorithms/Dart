7f5d2e2c860e4e2c9f8cf868527dbf856903fe20
Parowicz <arturparowicz@gmail.com>
2020-10-20 21:52:59 +0200

    Update swap_all_odd_and_even_bits.dart

--- other/swap_all_odd_and_even_bits.dart
+++ other/swap_all_odd_and_even_bits.dart
@@ -1,3 +1,5 @@
+import 'package:test/test.dart';
+
 //we are given an integer,the tast is to get its binary representation,swap all odd and even bits and print the new number
 int swapbits(int n) {
   return (((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1));
