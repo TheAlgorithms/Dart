ad6746e7389d108915d8281ccfd596fa15eb5efe
Ganesh Agarwal <69008792+Ganesh-Agarwal-25@users.noreply.github.com>
2020-10-26 11:38:13 +0530

    Update roman_to_integer.dart

--- conversions/roman_to_integer.dart
+++ conversions/roman_to_integer.dart
@@ -1,3 +1,22 @@
+int romanToInteger(var s) {
+  int ans = 0;
+  for (int i = 0; i < s.length; i++) {
+    int num1 = value(s[i]);
+    if (i + 1 < s.length) {
+      int num2 = value(s[i + 1]);
+      if (num1 < num2) {
+        ans = ans + num2 - num1;
+        i++;
+      } else {
+        ans = ans + num1;
+      }
+    } else {
+      ans = ans + num1;
+    }
+  }
+  return ans;
+}
+
 int value(var r) {
   if (r == 'I')
     return 1;
@@ -16,21 +35,18 @@ int value(var r) {
 }
 
 void main() {
-  String s = 'newnewnew';
-
-  int ans = 0;
-  for (int i = 0; i < s.length; i++) {
-    int num1 = value(s[i]);
-    if (i + 1 < s.length) {
-      int num2 = value(s[i + 1]);
-      if (num1 < num2) {
-        ans = ans + num2 - num1;
-        i++;
-      } else {
-        ans = ans + num1;
-      }
-    } else {
-      ans = ans + num1;
-    }
-  }
+  String s = 'XII';
+  print(romanToInteger(s));
+  s = 'LII';
+  print(romanToInteger(s));
+  s = 'DLVII';
+  print(romanToInteger(s));
+  s = 'VI';
+  print(romanToInteger(s));
+  s = 'CLXV';
+  print(romanToInteger(s));
+  s = 'MDCI';
+  print(romanToInteger(s));
+  s = 'LVII';
+  print(romanToInteger(s));
 }
