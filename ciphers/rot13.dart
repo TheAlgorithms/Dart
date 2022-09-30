36cb3c1d97ce444eec986836bae438a4dd6a387b
Sutthinart Khunvadhana <iakgoog@gmail.com>
2022-10-01 06:55:01 +0700

    update ROT13 wiki

--- ciphers/rot13.dart
+++ ciphers/rot13.dart
@@ -1,12 +1,17 @@
+// ROT13
+// https://en.wikipedia.org/wiki/ROT13
+
 import 'package:match/match.dart';
 import 'package:test/test.dart';
 
 String rot13(String s) {
-  return s.runes.map((d) => d.match({
-    range(97, 109) | range(65, 77): () => String.fromCharCode(d + 13),
-    range(110, 122) | range(78, 90): () => String.fromCharCode(d - 13),
-    any: () => String.fromCharCode(d),
-  })).join();
+  return s.runes
+      .map((d) => d.match({
+            range(97, 109) | range(65, 77): () => String.fromCharCode(d + 13),
+            range(110, 122) | range(78, 90): () => String.fromCharCode(d - 13),
+            any: () => String.fromCharCode(d),
+          }))
+      .join();
 }
 
 void main() {
@@ -15,7 +20,8 @@ void main() {
   });
 
   test("every alphabet with space", () {
-    expect(rot13("The quick brown fox jumps over the lazy dog"), equals("Gur dhvpx oebja sbk whzcf bire gur ynml qbt"));
+    expect(rot13("The quick brown fox jumps over the lazy dog"),
+        equals("Gur dhvpx oebja sbk whzcf bire gur ynml qbt"));
   });
 
   test("non alphabet", () {
