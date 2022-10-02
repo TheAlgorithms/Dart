d8c24b1ae285e23a9d0e24ffb6271391ed44b780
Jeevan Chandra Joshi <joshijeevan98@gmail.com>
2022-10-02 17:11:30 +0530

    Create sol20.dart

---
+++ project_euler/problem_20/sol20.dart
@@ -0,0 +1,32 @@
+/*
+Problem 20: Factorial digit sum 
+
+n! means n × (n − 1) × ... × 3 × 2 × 1
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+Find the sum of the digits in the number 100!
+*/
+
+// To Calculate Factorial
+int factorial(int number) {
+  int factorial = 1;
+  for (int i = number; i >= 1; i--) {
+    factorial *= i;
+  }
+  return factorial;
+}
+
+// To Calcuate Sum
+int sum(int number) {
+  int sum = 0;
+  for (int i = factorial(number); i > 0; i = (i / 10).floor()) {
+    sum += (i % 10);
+  }
+  return sum;
+}
+
+// Driver Code
+void main() {
+  int number = 100;
+  print(sum(number));
+}
