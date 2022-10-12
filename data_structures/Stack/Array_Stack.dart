2678893081cf6a79274efd301512807c469fed41
Akash G Krishnan <78728996+akashgk@users.noreply.github.com>
2022-10-12 15:55:30 +0530

    Fix test cases for array stack

--- data_structures/Stack/Array_Stack.dart
+++ data_structures/Stack/Array_Stack.dart
@@ -1,63 +1,94 @@
-//Author: Shawn
-//Email: stepfencurryxiao@gmail.com
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
 
 class ArrayStack<T> {
-  //stack
-  List<T> stack;
-  //element of the stack
-  int count;
-  //size of stack
-  int n;
+  /// [stack]
+  List<T> _stack;
+
+  /// [_count] is the number of element in the stack
+  int _count;
+
+  /// [_size] of stack
+  int _size;
 
   //Init the array stack
-  ArrayStack(var n) {
-    this.n = n;
-    this.stack = new List<T>(n);
-    this.count = 0;
+  ArrayStack(int size) {
+    this._size = size;
+    this._stack = List<T>.filled(_size, null);
+    this._count = 0;
   }
 
-  //Push a item to the stack
+  /// Push a item to the stack of type [T] to the [_stack]
+  /// if the size is exceeded the element wont be added.
   void push(T item) {
-    if (count == n) {
-      print("The stack is full\n");
+    if (_count == _size) {
+      return null;
     }
-    stack[count] = item;
-    count++;
+    _stack[_count] = item;
+    _count++;
   }
 
-  //Pop a item from the stack
+  /// Pop the last element inserted from the [_stack].
   T pop() {
-    if (count == 0) {
-      print("No data in the stack!\n");
+    if (_count == 0) {
+      return null;
     }
-    T pop_data = stack[count - 1];
-    stack[count - 1] = null;
-    count--;
+    T pop_data = _stack[_count - 1];
+    _stack[_count - 1] = null;
+    _count--;
     return pop_data;
   }
 
-  void Display() {
-    print("ArrayStack: $stack\n");
+  List<T> get stack {
+    return _stack;
   }
 }
 
 void main() {
-  ArrayStack<String> array_stack = new ArrayStack<String>(6);
+  ArrayStack<String> arrayStack = new ArrayStack<String>(6);
 
-  array_stack.push('1');
-  array_stack.push("2");
-  array_stack.push('3');
-  array_stack.push("4");
-  array_stack.push('5');
-  array_stack.push("6");
+  arrayStack.push('1');
+  arrayStack.push("2");
+  arrayStack.push('3');
+  arrayStack.push("4");
+  arrayStack.push('5');
+  arrayStack.push("6");
 
-  array_stack.Display();
+  test('test case 1', () {
+    expect(arrayStack.stack, ['1', '2', '3', '4', '5', '6']);
+  });
 
-  var pop_data;
-  pop_data = array_stack.pop();
-  print("Pop $pop_data from stack\n");
-  pop_data = array_stack.pop();
-  print("Pop $pop_data from stack\n");
-  print("Now the stock:");
-  array_stack.Display();
+  test('test case 2: pop stack', () {
+    expect('6', arrayStack.pop());
+  });
+
+  test('test case 3: pop stack', () {
+    expect('5', arrayStack.pop());
+  });
+
+  test('test case 4: pop stack', () {
+    expect('4', arrayStack.pop());
+  });
+
+  test('test case 5: pop stack', () {
+    expect('3', arrayStack.pop());
+  });
+
+  test('test case 6: pop stack', () {
+    expect('2', arrayStack.pop());
+  });
+
+  test('test case 7: pop stack', () {
+    expect('1', arrayStack.pop());
+  });
+
+  test('test case 8: pop stack', () {
+    expect(null, arrayStack.pop());
+  });
+
+  ArrayStack<String> arrayStack2 = new ArrayStack<String>(3);
+
+  test('test case 9', () {
+    expect(arrayStack2.stack, [null, null, null]);
+  });
 }
