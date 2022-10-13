a968df75ea6b0f16268577731a283bad6c99d6ee
Akash G Krishnan <78728996+akashgk@users.noreply.github.com>
2022-10-13 18:21:27 +0530

    Dart lint fixes

--- conversions/binary_to_decimal.dart
+++ conversions/binary_to_decimal.dart
@@ -1,30 +1,29 @@
 import "dart:math" show pow;
 
+int binaryToDecimal(String binaryString) {
+  binaryString = binaryString.trim();
+  if (binaryString == null || binaryString == "") {
+    throw Exception("An empty value was passed to the function");
+  }
+  bool isNegative = binaryString[0] == "-";
+  if (isNegative) binaryString = binaryString.substring(1);
+  int decimalValue = 0;
+  for (int i = 0; i < binaryString.length; i++) {
+    if ("01".contains(binaryString[i]) == false) {
+      throw Exception("Non-binary value was passed to the function");
+    } else {
+      decimalValue += pow(2, binaryString.length - i - 1) * int.parse((binaryString[i]));
+    }
+  }
+  return isNegative ? -1 * decimalValue : decimalValue;
+}
+
 void main() {
-  print(binary_to_decimal("-111")); // -7
-  print(binary_to_decimal(" 101011 ")); // 43
+  print(binaryToDecimal("-111")); // -7
+  print(binaryToDecimal(" 101011 ")); // 43
   try {
-    print(binary_to_decimal("1a1")); //error
+    print(binaryToDecimal("1a1")); //error
   } catch (ex) {
     print(ex);
   }
-}
-
-int binary_to_decimal(String bin_string) {
-  bin_string = bin_string.trim();
-  if (bin_string == null || bin_string == "") {
-    throw Exception("An empty value was passed to the function");
-  }
-  bool is_negative = bin_string[0] == "-";
-  if (is_negative) bin_string = bin_string.substring(1);
-  int decimal_val = 0;
-  for (int i = 0; i < bin_string.length; i++) {
-    if ("01".contains(bin_string[i]) == false) {
-      throw Exception("Non-binary value was passed to the function");
-    } else {
-      decimal_val +=
-          pow(2, bin_string.length - i - 1) * int.parse((bin_string[i]));
-    }
-  }
-  return is_negative ? -1 * decimal_val : decimal_val;
 }
