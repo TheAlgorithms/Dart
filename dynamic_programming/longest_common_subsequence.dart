d84024e7d1c2bb6fcee59a570cf85ffd24e4a1c5
Rohit <RohitSinghEmail@protonmail.com>
2022-10-18 12:42:39 +0530

    added dp alogorithm longest common subsequence in dart

--- dynamic_programming/longest_common_subsequence.dart
+++ dynamic_programming/longest_common_subsequence.dart
@@ -1,23 +1,17 @@
+// Longest Common Subsequence Editorial: https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
+//Longest Common Subsequence Wiki: https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
 import 'dart:math';
 import 'package:test/test.dart';
 
-// Prints the length of the longest common subsequence
-int longest_common_subsequence(String t1, String t2) {
-  int m = t1.length;
-  int n = t2.length;
+int longestCommonSubsequence(String text1, String text2) {
+  int m = text1.length;
+  int n = text2.length;
 
   // BottomUp Tabulation
-  List<List<int>> dp = List<List<int>>.filled(
-      m + 1, List<int>.filled(n + 1, 0, growable: false),
-      growable: false);
-
+  List<List<int>> dp = List.generate(m + 1, (_) => List<int>.filled(n + 1, 0));
   for (int i = 1; i <= m; i++) {
     for (int j = 1; j <= n; j++) {
-      // if (i == 0 || j == 0) {
-      //   dp[i][j] = 0;
-      //   continue;
-      // }
-      if (t1[i - 1] == t2[j - 1]) {
+      if (text1[i - 1] == text2[j - 1]) {
         dp[i][j] = 1 + dp[i - 1][j - 1];
         continue;
       }
@@ -29,34 +23,34 @@ int longest_common_subsequence(String t1, String t2) {
 
 void main() {
   test(('testCase #1'), () {
-    expect(longest_common_subsequence("", ""), equals(0));
+    expect(longestCommonSubsequence("", ""), equals(0));
   });
 
   test(('testCase #2'), () {
-    expect(longest_common_subsequence("", "abcd"), equals(0));
+    expect(longestCommonSubsequence("", "abcd"), equals(0));
   });
   test(('testCase #3'), () {
-    expect(longest_common_subsequence("abcd", ""), equals(0));
+    expect(longestCommonSubsequence("abcd", ""), equals(0));
   });
   test(('testCase #4'), () {
-    expect(longest_common_subsequence("abcd", "c"), equals(1));
+    expect(longestCommonSubsequence("abcd", "c"), equals(1));
   });
   test(('testCase #5'), () {
-    expect(longest_common_subsequence("abcd", "d"), equals(1));
+    expect(longestCommonSubsequence("abcd", "d"), equals(1));
   });
   test(('testCase #6'), () {
-    expect(longest_common_subsequence("abcd", "e"), equals(0));
+    expect(longestCommonSubsequence("abcd", "e"), equals(0));
   });
   test(('testCase #7'), () {
-    expect(longest_common_subsequence("abcdefghi", "acegi"), equals(5));
+    expect(longestCommonSubsequence("abcdefghi", "acegi"), equals(5));
   });
   test(('testCase #8'), () {
-    expect(longest_common_subsequence("abcdgh", "aedfhr"), equals(3));
+    expect(longestCommonSubsequence("abcdgh", "aedfhr"), equals(3));
   });
   test(('testCase #9'), () {
-    expect(longest_common_subsequence("aggtab", "gxtxayb"), equals(4));
+    expect(longestCommonSubsequence("aggtab", "gxtxayb"), equals(4));
   });
   test(('testCase #10'), () {
-    expect(longest_common_subsequence("ggg", "g"), equals(1));
+    expect(longestCommonSubsequence("ggg", "g"), equals(1));
   });
 }
