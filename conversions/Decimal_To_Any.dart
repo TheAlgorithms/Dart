d591c30caeefd315ec7bcb89a5e1d54779288f69
Akash G Krishnan <78728996+akashgk@users.noreply.github.com>
2022-12-15 21:34:00 +0530

    Update Decimal_To_Any.dart

--- conversions/Decimal_To_Any.dart
+++ conversions/Decimal_To_Any.dart
@@ -1,25 +1,10 @@
 //Convert a Decimal Number to Any Other Representation
 //https://en.wikipedia.org/wiki/Positional_notation#Base_conversion
 
-void main() {
-  print(decimal_to_any(0, 2)); //Expected '0'
-  print(decimal_to_any(5, 4)); //Expected '11'
-  print(decimal_to_any(20, 3)); //Expected '202'
-  print(decimal_to_any(-58, 16)); //Expected '-3A'
-  print(decimal_to_any(243, 17)); //Expected 'E5'
-  print(decimal_to_any(34923, 36)); //Expected 'QY3'
-  print(decimal_to_any(10, 11)); //Expected 'A'
-  print(decimal_to_any(-16, 16)); //Expected '-10'
-  print(decimal_to_any(36, 36)); //Expected '10'
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
 
-  try {
-    print(decimal_to_any(10, 37)); //Expected Error
-  } on FormatException {
-    print("Base value is not supported");
-  }
-}
-
-String decimal_to_any(int value, int base) {
+String decimalToAny(int value, int base) {
   var ALPHABET_VALUES = {
     10: 'A',
     11: 'B',
@@ -64,9 +49,20 @@ String decimal_to_any(int value, int base) {
     int remainder = value % base;
     value = value ~/ base;
     output =
-        (remainder < 10 ? remainder.toString() : ALPHABET_VALUES[remainder]) +
-            output;
+        (remainder < 10 ? remainder.toString() : ALPHABET_VALUES[remainder]) + output;
   }
 
   return negative ? '-' + output : output;
+}
+
+void main() {
+  test('test case 1', () => expect(decimalToAny(0, 2), '0'));
+  test('test case 2', () => expect(decimalToAny(5, 4), '11'));
+  test('test case 3', () => expect(decimalToAny(20, 3), '202'));
+  test('test case 4', () => expect(decimalToAny(-58, 16), '-3A'));
+  test('test case 5', () => expect(decimalToAny(243, 17), 'E5'));
+  test('test case 6', () => expect(decimalToAny(34923, 36), 'QY3'));
+  test('test case 7', () => expect(decimalToAny(10, 11), 'A'));
+  test('test case 8', () => expect(decimalToAny(-16, 16), '-10'));
+  test('test case 9', () => expect(decimalToAny(36, 36), '10'));
 }
