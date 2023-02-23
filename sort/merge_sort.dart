049d98eb5c7ec9b87edd8868d66ec0526282e55c
Akash G Krishnan <78728996+akashgk@users.noreply.github.com>
2023-02-23 23:53:51 +0530

    Revert "Add test case to merge sort"

--- sort/merge_sort.dart
+++ sort/merge_sort.dart
@@ -1,11 +1,11 @@
-import 'package:test/test.dart';
+import 'dart:math' show Random;
 
 void merge(List list, int lIndex, int mIndex, int rIndex) {
   int lSize = mIndex - lIndex + 1;
   int rSize = rIndex - mIndex;
 
-  List lList = new List.filled(lSize, () => 0);
-  List rList = new List.filled(rSize, () => 0);
+  List lList = new List(lSize);
+  List rList = new List(rSize);
 
   for (int i = 0; i < lSize; i++) lList[i] = list[lIndex + i];
   for (int j = 0; j < rSize; j++) rList[j] = list[mIndex + j + 1];
@@ -37,7 +37,7 @@ void merge(List list, int lIndex, int mIndex, int rIndex) {
   }
 }
 
-List<int> mergeSort(List<int> list, int lIndex, int rIndex) {
+void mergeSort(List list, int lIndex, int rIndex) {
   if (lIndex < rIndex) {
     int mIndex = (rIndex + lIndex) ~/ 2; // finds the middle index
 
@@ -46,30 +46,16 @@ List<int> mergeSort(List<int> list, int lIndex, int rIndex) {
 
     merge(list, lIndex, mIndex, rIndex);
   }
-  return list;
 }
 
 void main() {
-  List<int> list = [5, 4, 3, 2, 1];
-  test('test case 1',
-      () => expect(mergeSort(list, 0, list.length - 1), [1, 2, 3, 4, 5]));
-
-  List<int> list1 = [];
-  test('test case 2', () => expect(mergeSort(list1, 0, list1.length - 1), []));
-
-  List<int> list2 = [1, 1, 1, 1, 1];
-  test('test case 3',
-      () => expect(mergeSort(list2, 0, list2.length - 1), [1, 1, 1, 1, 1]));
-
-  List<int> list3 = [-1, -11, -1221, -123121, -1111111];
-  test(
-      'test case 4',
-      () => expect(mergeSort(list3, 0, list3.length - 1),
-          [-1111111, -123121, -1221, -11, -1]));
-
-  List<int> list4 = [11, 1, 1200, -1, 5];
-  test(
-      'test case 1',
-      () =>
-          expect(mergeSort(list4, 0, list4.length - 1), [-1, 1, 5, 11, 1200]));
+  final seed = 100, rnd = Random(), length = 100;
+  var list =
+      List<int>.generate(length, (i) => rnd.nextInt(seed), growable: false);
+  print('before sorting:');
+  print(list);
+  print('--------------------------------------');
+  print('After sorting:');
+  mergeSort(list, 0, list.length - 1);
+  print(list);
 }
