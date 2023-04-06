070ad9e24a3e2f3038beedbe260bdbe69a278998
Dylan Hernandez <hernandez.dylan0622@gmail.com>
2023-04-06 13:11:02 -0700

    Checks input of factorial
    Makes sure that the "n" variable input is valid by checking if it is greater than or equal to zero and if it is indeed a whole number. If it is not, the program prints that "n" is an invalid input.

--- maths/factorial.dart
+++ maths/factorial.dart
@@ -1,7 +1,11 @@
 void main() {
   var n = 5;
-  var fac = factorial(n);
+  if(n>=0 && n.runtimeType == int){
+    var fac = factorial(n);
   print("$n! = $fac"); /* output: 5! = 120 */
+  }else{
+    print("Invalid input. Input has to be a positive whole integer");
+  }
 }
 
 /* calculate factorial of n*/
