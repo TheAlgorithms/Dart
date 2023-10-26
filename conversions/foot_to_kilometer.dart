2da2459bd46a24de9ebb7f2aa323a583dab1b9d1
Nenza Nurfirmansyah <nenza.dev@gmail.com>
2023-10-26 14:07:57 +0700

    add mile & foot to kilometer conversions

---
+++ conversions/foot_to_kilometer.dart
@@ -0,0 +1,10 @@
+double footToKilometer(double nums) {
+  double target = nums * 0.0003048;
+  print(target.toString());
+
+  return target;
+}
+
+void main() {
+  footToKilometer(8.1);
+}
---
+++ conversions/mile_to_kilometer.dart
@@ -0,0 +1,10 @@
+double mileToKilometer(double nums) {
+  double target = nums * 1.60934;
+  print(target.toString());
+
+  return target;
+}
+
+void main() {
+  mileToKilometer(8.1);
+}
